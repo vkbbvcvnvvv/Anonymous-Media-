7677498eb30fa75bc1bfb367e85d911c83008c03
vkbbvcvnvvv <94930978+vkbbvcvnvvv@users.noreply.github.com>
2023-01-28 13:30:19 +0200

    Add files via upload

---
+++ Anonymous_black_hat.py
@@ -0,0 +1,82 @@
+import os
+import time
+os.system("clear")
+#My_Banner_From_darkstorm✗
+print ("    \033[1;31m _            _        _\033[4;30m")
+print (" \033[1;31m __| | __ _ _ __| | _____| |_ ___  _ __ _ __ ___\033[4;30m")
+print (" \033[1;31m/ _` |/ _` | '__| |/ / __| __/ _ \| '__| '_ ` _ \ \033[4;30m")
+print ("\033[1;31m| (_| | (_| | |  |   <\__ \ || (_) | |  | | | | | |\033[4;30m")
+print (" \033[1;31m\__,_|\__,_|_|  |_|\_\___/\__\___/|_|  |_| |_| |_|\033[4;30m")
+print ("                      ")
+print ("                      ")
+print ("                      ")
+#------------------------------------------------------------------
+#Commend_メ
+print ("[1] install pkg \033[1;34m")
+time.sleep (0.3)
+print ("[2] install metasploit \033[1;34m")
+time.sleep (0.3)
+print ("[3] Install Kali in Termux \033[1;33m")
+time.sleep (0.3)
+print ("[4] install nikto in termux \033[1;33m")
+time.sleep (0.3)
+print ("[5] install whois in termux \033[1;33m")
+time.sleep (0.3)
+print ("[6] install TheHarvestar \033[1;33m")
+time.sleep (0.3)
+print ("[7]q install hammer \033[1;33m")
+time.sleep (0.3)
+print ("[8] Install Root in Turmax \033[1;34m")
+time.sleep (0.3)
+print ("[9] Install Termux Desktop \033[1;34m")
+rvqbeku2d2atime.sleep (0.3)
+#--------------------------------------
+
+nun = input ("Chose Nuber : ")
+
+if nun == "1" :
+        os.system("termux-setup-storage;cd;dpkg --configure -a;pkg update -y;pkg upgrade -y;pkg install python -y;pkg install python2 -y;pkg install python2-dev -y;pkg install python3 -y;pkg install pip -y;pkg install pip2;pip2 install requests;pkg install fish -y;pkg install ruby -y;pkg install git -y;pkg install dnsutils -y;pkg install php -y;pkg install perl -y;pkg install nmap -y;pkg install bash -y;pkg install clang -y;pkg install nano -y;pkg install w3m -y;pkg install figlet -y;pkg install cowsay -y;gem install lolcat;pkg install curl -y;pkg install tar -y;pkg install zip -y;pkg install unzip -y;pkg install tor -y;pkg install wget -y;pkg install wgetrc -y;pkg install wcalc -y;pkg install bmon -y;pkg install unrar -y;pkg install toilet -y;pkg install proot -y;pkg install golang -y;pkg install chroot -y;termux-chroot -y;pkg install openssl -y;pkg install cmatrix -y;pkg install openssh -y;apt update && apt upgrade -y")
+#______________________________________
+if nun == "2" :
+        os.system("apt update && apt upgrade -y")
+        os.system("apt install ruby")
+        print ("source <(curl -fsSL https://kutt.it/msf)")
+#______________________________________
+if nun == "3" :
+        os.system ("pkg install wget")
+        os.system ("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+        os.system ("chmod +x install-nethunter-termux")
+        os.system ("./install-nethunter-termux")
+#______________________________________
+if nun == "4" :
+        os.system ("apt update && apt upgrade -y")
+        os.system ("apt install git")
+        os.system ("pkg install perl -y")
+        os.system ("cd")
+        os.system ("git clone https://github.com/sullo/nikto")
+#______________________________________
+if nun == "5" :
+        os.system("apt install whois -y")
+#______________________________________
+if nun == "6" :
+        os.system("apt update && apt upgrade -y")
+        os.system("apt install git -y")
+        os.system("cd")
+        os.system("git clone https://github.com/makerdao/theharvester")
+#______________________________________
+if nun == "7" :
+        os.system("pkg update && pkg upgrade -y")
+        os.system("apt install python python2 python3 -y")
+        os.system("apt install git -y")
+        os.system("git clone https://github.com/Pavithran-R/Hammer/")
+#______________________________________
+if nun == "8" :
+        os.system ("cd")
+        os.system ("pkg install proot -y")
+        os.system ("git clone https://github.com/MFDGaming/ubuntu-in-termux.git")
+#______________________________________
+if nun == "9" :
+        os.system ("apt update && apt upgrade -y")
+        os.system ("apt install git")
+        os.system ("git clone https://github.com/adi1090x/termux-desktop")
+#darkstorm
